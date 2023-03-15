dd5f3989c65bed7e32fe07292d70c54db18e77fc
nilsukonak <115398987+nilsukonak@users.noreply.github.com>
2023-03-15 15:00:04 +0300

    Update soru3.c

--- soru3.c
+++ soru3.c
@@ -6,12 +6,13 @@
 int Search (int D[],int N,int sayi) 
 {
 	
-	int i =0;         // 1 
+	int i =0;         // 1 kere
 	while(i<N){       // n+1 kere 
-		if (D[i]==sayi)break;  //n*
-		i++;                   //n*
+		if (D[i]==sayi)break;  //while 1 kere çalıştığı zaman if,break,i++ 1 kere calışacak yani toplam 3 kere 
+		i++;                   // 3*(n+1)
 	}
-	if (i<N)return i ;
-	else return -1;
-
+	if (i<N)return i ; //if 1, return 1 kere çalışacak=2
+	else return -1; //return 1 kere çalışacak
+                          //  T(n)=4n+8 sabit sayıları atıyoruz n kalıyor. BigO'su O(n) yani cevap en iyi durum
+ 
 }
